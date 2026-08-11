715246c2c203e0a3725259377aeca2b3a3baa9e1
Prithvijit Guha <71138854+prithvijitguha@users.noreply.github.com>
2026-08-11 07:55:16 +0530

    moved github-cli, lazydocker, lazygit, zellij, docker, mise, select-dev-language, select-dev-storage to optional installs, removed some git aliases

--- install/terminal/set-git.sh
+++ install/terminal/set-git.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 # Set common git aliases
-git config --global alias.co checkout
-git config --global alias.br branch
-git config --global alias.ci commit
-git config --global alias.st status
 git config --global pull.rebase true
 
 # Set identification from install inputs
