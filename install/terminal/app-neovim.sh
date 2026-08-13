a1c5810857395932a0b127023a3166b9b68ee490
Prithvijit Guha <71138854+prithvijitguha@users.noreply.github.com>
2026-08-13 22:04:28 +0530

    updated nvim install default

--- install/terminal/app-neovim.sh
+++ install/terminal/app-neovim.sh
@@ -26,12 +26,6 @@ if [ ! -d "$HOME/.config/nvim" ]; then
   # Default to Tokyo Night theme
   cp ~/.local/share/omadeb/themes/tokyo-night/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
 
-  # Turn off animated scrolling
-  cp ~/.local/share/omadeb/configs/neovim/snacks-animated-scrolling-off.lua ~/.config/nvim/lua/plugins/
-
-  # Turn off relative line numbers
-  echo "vim.opt.relativenumber = false" >>~/.config/nvim/lua/config/options.lua
-
   # Ensure editor.neo-tree is used by default
   cp ~/.local/share/omadeb/configs/neovim/lazyvim.json ~/.config/nvim/
 fi
