49d6fa9fbaa13c00b72dd61e091e33278aa4aa1a
Prithvijit Guha <71138854+prithvijitguha@users.noreply.github.com>
2026-08-16 12:44:50 +0530

    Fixed fastfetch install and ulauncher

--- install/terminal/app-fastfetch.sh
+++ install/terminal/app-fastfetch.sh
@@ -2,9 +2,9 @@
 
 if command -v fastfetch >/dev/null 2>&1; then
   # Display system information in the terminal
-  wget "https://github.com/fastfetch-cli/fastfetch/releases/download/2.67.1/fastfetch-linux-aarch64.deb"
-  sudo apt install -y ./fastfetch-linux-aarch64.deb
-  rm fastfetch-linux-aarch64.deb
+  wget "https://github.com/fastfetch-cli/fastfetch/releases/download/2.67.1/fastfetch-linux-amd64.deb"
+  sudo apt install -y ./fastfetch-linux-amd64.deb
+  rm fastfetch-linux-amd64.deb
 fi
 
 if [ ! -f "$HOME/.config/fastfetch/config.jsonc" ]; then
