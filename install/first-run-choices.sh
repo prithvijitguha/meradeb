28215dcdc80e9986c2cea7bb709c0ee8f5a14c06
Prithvijit Guha <71138854+prithvijitguha@users.noreply.github.com>
2026-08-16 12:57:21 +0530

    Updated first run choices permissions

--- install/first-run-choices.sh
+++ install/first-run-choices.sh
@@ -3,6 +3,6 @@
 # Only ask for default desktop app choices when running Gnome
 if [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]]; then
   OPTIONAL_APPS=("1password" "Spotify" "Zoom" "Dropbox")
-  DEFAULT_OPTIONAL_APPS=''
+  DEFAULT_OPTIONAL_APPS='1password,Spotify,Zoom'
   export omadeb_FIRST_RUN_OPTIONAL_APPS=$(gum choose "${OPTIONAL_APPS[@]}" --no-limit --selected $DEFAULT_OPTIONAL_APPS --height 7 --header "Select optional apps" | tr ' ' '-')
 fi
