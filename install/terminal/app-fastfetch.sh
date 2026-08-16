283b5a42af73f43ff7535be9f4e219072d9fa958
Prithvijit Guha <71138854+prithvijitguha@users.noreply.github.com>
2026-08-16 12:37:14 +0530

    Fixed fastfetch install

--- install/terminal/app-fastfetch.sh
+++ install/terminal/app-fastfetch.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# Display system information in the terminal
-wget -o "https://github.com/fastfetch-cli/fastfetch/releases/download/2.67.1/fastfetch-linux-aarch64.deb"
-sudo apt install -y ./fastfetch-linux-aarch64.deb
-rm fastfetch-linux-aarch64.deb
+if command -v fastfetch >/dev/null 2>&1; then
+  # Display system information in the terminal
+  wget -o "https://github.com/fastfetch-cli/fastfetch/releases/download/2.67.1/fastfetch-linux-aarch64.deb"
+  sudo apt install -y ./fastfetch-linux-aarch64.deb
+  rm fastfetch-linux-aarch64.deb
+fi
 
 if [ ! -f "$HOME/.config/fastfetch/config.jsonc" ]; then
   # Use omadeb fastfetch config
