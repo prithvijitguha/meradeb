90c63d39c4f63695a58461a4f8a43e1956b720ea
Prithvijit Guha <71138854+prithvijitguha@users.noreply.github.com>
2026-08-16 09:08:12 +0530

    Added wofi installer

--- install/desktop/app-wofi.sh
+++ install/desktop/app-wofi.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+sudo apt update && sudo apt install build-essential meson ninja-build pkg-config
+git clone --depth 1 https://github.com/SimplyCEO/wofi.git /tmp/wofi
+cd wofi
+meson setup build
+ninja -C build
+su -c "ninja -C build install"
