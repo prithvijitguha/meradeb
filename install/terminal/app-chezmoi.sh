f28d19b593752371f97580a22a8ca223c44a6798
Prithvijit Guha <71138854+prithvijitguha@users.noreply.github.com>
2026-08-16 10:51:12 +0530

    Added check if command exists

--- install/terminal/app-chezmoi.sh
+++ install/terminal/app-chezmoi.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sh -c "$(curl -fsLS https://get.chezmoi.io)" -- -b "$HOME/.local/bin"
+if command -v chezmoi >/dev/null 2>&1; then
+  sh -c "$(curl -fsLS https://get.chezmoi.io)" -- -b "$HOME/.local/bin"
+fi
