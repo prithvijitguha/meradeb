56af9bd4aee26caeaefc2dde08b82a404c3b4eb4
Prithvijit Guha <71138854+prithvijitguha@users.noreply.github.com>
2026-08-16 13:11:21 +0530

    Updated install required libraries

--- install/desktop/app-wofi.sh
+++ install/desktop/app-wofi.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 if command -v wofi >/dev/null 2>&1; then
-  sudo apt update && sudo apt install build-essential meson ninja-build pkg-config
   git clone --depth 1 https://github.com/SimplyCEO/wofi.git /tmp/wofi
   cd wofi
   meson setup build
