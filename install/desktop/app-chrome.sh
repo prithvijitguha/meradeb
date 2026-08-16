cb20edecec376ee464add99fc4f9c05feef641c7
Prithvijit Guha <prithvijit.guha@outlook.com>
2026-08-16 20:03:31 +0530

    Updated chrome install

--- install/desktop/app-chrome.sh
+++ install/desktop/app-chrome.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Browse the web with the most popular browser. See https://www.google.com/chrome/
-if command -v google-chrome >/dev/null 2>&1; then
+if ! command -v google-chrome >/dev/null 2>&1; then
   cd /tmp
   wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   sudo apt install -y ./google-chrome-stable_current_amd64.deb
