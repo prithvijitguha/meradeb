a3b3bb9fb41d25b0eb27446e94be7c2a75d0e625
Prithvijit Guha <prithvijit.guha@outlook.com>
2026-08-17 19:06:55 +0530

    Fixed youtube music

--- applications/YouTube-Music.sh
+++ applications/YouTube-Music.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-mkdir -p $HOME/.local/share/application/
-mkdir -p $HOME/.local/share/youtube-music/icons/
-
-cp setup/icons/music.png /home/$USER/.local/share/youtube-music/icons/music.png
-
-cat <<EOF >$HOME/.local/share/applications/chrome-music.youtube.com__-Default.desktop
+cat <<EOF >~/.local/share/applications/chrome-music.youtube.com__-Default.desktop
 [Desktop Entry]
 Version=1.0
 Name=YouTube Music
@@ -13,7 +8,7 @@ Comment=YouTube Music
 Exec=google-chrome --app="https://music.youtube.com" --class=YouTubeMusic
 Terminal=false
 Type=Application
-Icon=/home/$USER/.local/share/youtube-music/icons/music.png
+Icon=music
 Categories=GTK;
 StartupNotify=true
 StartupWMClass=google-chrome
