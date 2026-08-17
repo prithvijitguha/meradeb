2342dad0842f26f5e7160de31f1bf271acb4a66d
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-08-17 21:05:27 +0530

    Fixed steam installation

--- install/desktop/app-steam.sh
+++ install/desktop/app-steam.sh
@@ -2,7 +2,5 @@
 
 # Play games from https://store.steampowered.com/
 
-if ! command -v google-chrome >/dev/null 2>&1; then
-  sudo apt install mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386
-  sudo apt install steam-installer
-fi
+sudo apt install -y mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386
+sudo apt install -y steam-installer
