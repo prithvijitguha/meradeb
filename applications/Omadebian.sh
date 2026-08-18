1a934454666db406e63cae059b3a7644a82541a8
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-08-18 20:02:55 +0530

    Updated omadebian menu

--- applications/Omadebian.sh
+++ applications/Omadebian.sh
@@ -5,7 +5,7 @@ cat <<EOF >"$HOME/.local/share/applications/Omadebian.desktop"
 Version=1.0
 Name=Omadebian
 Comment=Omadebian Controls
-Exec=ghostty --class=Omadeb --title=Omadebian -e zsh -lic 'export OMADEB_PATH="$HOME/.local/share/omadeb"; export PATH="$HOME/.local/share/omadeb/bin:$HOME/.local/bin:$PATH"; omadeb'
+Exec=ghostty --class=Omadeb --title=Omadebian --window-width=800 --window-height=600 -e zsh -lic 'export OMADEB_PATH="$HOME/.local/share/omadeb"; export PATH="$HOME/.local/share/omadeb/bin:$HOME/.local/bin:$PATH"; omadeb'
 Terminal=false
 Type=Application
 Icon=$HOME/.local/share/omadeb/applications/icons/Omadeb.png
