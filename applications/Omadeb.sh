0c77d913148ea027373190a6577f5a6e20a7874c
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-08-18 07:48:20 +0530

    Fixes for omadeb

--- applications/Omadeb.sh
+++ applications/Omadeb.sh
@@ -5,7 +5,7 @@ cat <<EOF >"$HOME/.local/share/applications/Omadeb.desktop"
 Version=1.0
 Name=Omadeb
 Comment=Omadeb Controls
-Exec=ghostty --class=Omadeb --title=Omadeb -e zsh -lc 'omadeb'
+Exec=ghostty --class=Omadeb --title=Omadeb -e zsh -lic 'export OMADEB_PATH="$HOME/.local/share/omadeb"; export PATH="$HOME/.local/share/omadeb/bin:$HOME/.local/bin:$PATH"; omadeb'
 Terminal=false
 Type=Application
 Icon=$HOME/.local/share/omadeb/applications/icons/Omadeb.png
