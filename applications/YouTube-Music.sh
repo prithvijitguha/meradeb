0a19a357b49715d2a13f0ce8c8c993909097c698
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-08-18 17:56:07 +0530

    Updated youtube music to firefox instead

--- applications/YouTube-Music.sh
+++ applications/YouTube-Music.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-cat <<EOF >~/.local/share/applications/chrome-music.youtube.com__-Default.desktop
+cat <<EOF >~/.local/share/applications/firefox-youtube-music.desktop
 [Desktop Entry]
 Version=1.0
 Name=YouTube Music
 Comment=YouTube Music
-Exec=google-chrome --app="https://music.youtube.com" --class=YouTubeMusic
+Exec=firefox --new-window "https://music.youtube.com"
 Terminal=false
 Type=Application
 Icon=$HOME/.local/share/omadeb/applications/icons/Music.png
 Categories=GTK;
 StartupNotify=true
-StartupWMClass=google-chrome
+StartupWMClass=firefox
 EOF
