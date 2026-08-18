2da98cde34d73cae57acc3163d791f7e4db4e962
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-08-18 18:00:52 +0530

    test with chromium

--- applications/YouTube-Music.sh
+++ applications/YouTube-Music.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-cat <<EOF >~/.local/share/applications/chrome-music.youtube.com__-Default.desktop
+cat <<EOF >~/.local/share/applications/chromium-music.youtube.com.desktop
 [Desktop Entry]
 Version=1.0
 Name=YouTube Music
 Comment=YouTube Music
-Exec=google-chrome --app="https://music.youtube.com" --class=YouTubeMusic
+Exec=chromium --app="https://music.youtube.com" --class=YouTubeMusic
 Terminal=false
 Type=Application
 Icon=$HOME/.local/share/omadeb/applications/icons/Music.png
 Categories=GTK;
 StartupNotify=true
-StartupWMClass=google-chrome
+StartupWMClass=chromium
 EOF
