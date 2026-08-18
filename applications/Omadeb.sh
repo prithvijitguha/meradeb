c7576de08200f486d557a536524759441283d1ef
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-08-18 07:41:39 +0530

    Updated omadeb

--- applications/Omadeb.sh
+++ applications/Omadeb.sh
@@ -5,7 +5,7 @@ cat <<EOF >"$HOME/.local/share/applications/Omadeb.desktop"
 Version=1.0
 Name=Omadeb
 Comment=Omadeb Controls
-Exec=ghostty --class=Omadeb --title=Omadeb -e omadeb
+Exec=ghostty --class=Omadeb --title=Omadeb -e zsh -lc 'omadeb'
 Terminal=false
 Type=Application
 Icon=$HOME/.local/share/omadeb/applications/icons/Omadeb.png
