750f0e74e8e2258d4000fdf00ab4a21ce4d605c4
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-08-18 07:25:29 +0530

    Added overwrite and bootsh log statement

--- install/terminal/a-shell.sh
+++ install/terminal/a-shell.sh
@@ -2,11 +2,11 @@
 
 # Configure the bash shell using omadeb defaults
 [ -f ~/.bashrc ] && mv ~/.bashrc ~/.bashrc.bak
-\cp ~/.local/share/omadeb/configs/bashrc ~/.bashrc
+yes | cp ~/.local/share/omadeb/configs/bashrc ~/.bashrc
 
 # Load the PATH for use later in the installers
 source ~/.local/share/omadeb/defaults/bash/shell
 
 [ -f ~/.inputrc ] && mv ~/.inputrc ~/.inputrc.bak
 # Configure the inputrc using omadeb defaults
-\cp ~/.local/share/omadeb/configs/inputrc ~/.inputrc
+yes | cp ~/.local/share/omadeb/configs/inputrc ~/.inputrc
