ff5b12e5f9396062000eb6d41fa5ed295abaa0d0
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-08-18 10:46:11 +0530

    Application name to omadebian

---
+++ applications/Omadebian.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat <<EOF >"$HOME/.local/share/applications/Omadebian.desktop"
+[Desktop Entry]
+Version=1.0
+Name=Omadebian
+Comment=Omadebian Controls
+Exec=ghostty --class=Omadeb --title=Omadebian -e zsh -lic 'export OMADEB_PATH="$HOME/.local/share/omadeb"; export PATH="$HOME/.local/share/omadeb/bin:$HOME/.local/bin:$PATH"; omadeb'
+Terminal=false
+Type=Application
+Icon=$HOME/.local/share/omadeb/applications/icons/Omadeb.png
+Categories=GTK;
+StartupNotify=false
+EOF
