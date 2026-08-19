9b9bb6140e0a674ada57af9d6a2746a5648f9076
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-08-19 09:37:12 +0530

    Fixed btop timezone issue

--- applications/Activity.sh
+++ applications/Activity.sh
@@ -5,7 +5,7 @@ cat <<EOF >~/.local/share/applications/Activity.desktop
 Version=1.0
 Name=Activity
 Comment=System activity from btop
-Exec=ghostty --class=Activity --title=Activity -e btop
+Exec=env LANG=en_US.UTF-8 LC_CTYPE=en_US.UTF-8 ghostty --class=Activity --title=Activity -e btop
 Terminal=false
 Type=Application
 Icon=/home/$USER/.local/share/omadeb/applications/icons/Activity.png
