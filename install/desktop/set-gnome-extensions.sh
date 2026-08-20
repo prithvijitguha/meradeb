e667fe9d6caeb7bb5a920a0f82f0613266d84a80
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-08-20 20:34:12 +0530

    Updated package list and gnome extensions

--- install/desktop/set-gnome-extensions.sh
+++ install/desktop/set-gnome-extensions.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sudo apt install -y gnome-shell-extension-manager gir1.2-gtop-2.0 gir1.2-clutter-1.0
 pipx install gnome-extensions-cli --system-site-packages
 
 # Install new extensions
--- install/install-base-packages.sh
+++ install/install-base-packages.sh
@@ -3,6 +3,6 @@ sudo apt update -y
 sudo apt upgrade -y
 
 # Then install all other files
-sudo apt install -y $(cat pkglist)
+sudo apt install -y $(cat ~/.local/share/omadeb/install/omadebian-base-packages.txt)
 # Run terminal installers
 for installer in ~/.local/share/omadeb/install/terminal/*.sh; do source $installer; done
