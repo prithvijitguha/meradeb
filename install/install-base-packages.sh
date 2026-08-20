e03c375ac9b51f0dcc2c0ad1bb7829aed308e4a1
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-08-20 20:22:16 +0530

    Updated default configs

--- install/install-base-packages.sh
+++ install/install-base-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install -y curl git unzip build-essential meson ninja-build pkg-config firefox-esr rustup bat tmux fzf ripgrep bat eza zoxide plocate apache2-utils fd-find
+sudo apt install -y curl git unzip build-essential meson ninja-build pkg-config firefox-esr rustup bat tmux fzf ripgrep bat eza zoxide plocate apache2-utils fd-find btop
 # Run terminal installers
 for installer in ~/.local/share/omadeb/install/terminal/*.sh; do source $installer; done
