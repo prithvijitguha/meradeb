9f81791b434e800912f492d893dc96ed794073bf
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-08-20 20:30:38 +0530

    Moved to package list

--- install/install-base-packages.sh
+++ install/install-base-packages.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install -y
 
 # Then install all other files
-$(cat pkglist)
+sudo apt install -y $(cat pkglist)
 # Run terminal installers
 for installer in ~/.local/share/omadeb/install/terminal/*.sh; do source $installer; done
