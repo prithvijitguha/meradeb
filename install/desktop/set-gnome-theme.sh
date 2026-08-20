95d2dd1040fd2094ce415bdeeeae9bd822874eb7
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-08-20 20:49:40 +0530

    Changed default theme after setup

--- install/desktop/set-gnome-theme.sh
+++ install/desktop/set-gnome-theme.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-source ~/.local/share/omadeb/themes/tokyo-night/gnome.sh
+source ~/.local/share/omadeb/themes/everforest/gnome.sh
 source ~/.local/share/omadeb/themes/tokyo-night/tophat.sh
