3540bda094421df1c61971ae8f4b7f6af34f59a7
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-08-20 20:28:39 +0530

    Moved to package list

--- install/install-base-packages.sh
+++ install/install-base-packages.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install -y curl git unzip build-essential meson ninja-build pkg-config firefox-esr rustup bat tmux fzf ripgrep bat eza zoxide plocate apache2-utils fd-find btop
+sudo apt install -y
+
+# Then install all other files
+$(cat pkglist)
 # Run terminal installers
 for installer in ~/.local/share/omadeb/install/terminal/*.sh; do source $installer; done
