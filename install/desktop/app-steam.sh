72c46551c04e368bbe2c51c64b21d0a9c923d320
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-08-20 20:40:56 +0530

    Updated installation packages

--- install/desktop/app-steam.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Play games from https://store.steampowered.com/
-
-sudo apt install -y mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386
-sudo apt install -y steam-installer
