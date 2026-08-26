d78e5afa83ea6904b5a1c1c9920892de8048de40
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-08-26 21:45:10 +0530

    Updated boot references for git

--- boot.sh
+++ boot.sh
@@ -15,8 +15,6 @@ echo -e "$ascii_art"
 echo "=> Warning: Meradeb is recommeded for Debian 13 Trixie setups"
 echo -e "\nBegin installation (or abort with ctrl+c)..."
 
-sudo apt update && sudo apt install git
-
 echo "Cloning Meradeb..."
 rm -rf ~/.local/share/meradeb
 git clone git@github.com:prithvijitguha/meradeb.git ~/.local/share/meradeb >/dev/null
