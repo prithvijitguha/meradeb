35b731e7344c9bdefc31b5b410b095ba06344588
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-08-26 22:52:08 +0530

    Updated git settings

--- install/terminal/set-git.sh
+++ install/terminal/set-git.sh
@@ -2,12 +2,3 @@
 
 # Set common git aliases
 git config --global pull.rebase true
-
-# Set identification from install inputs
-if [[ -n "${MERADEB_USER_NAME//[[:space:]]/}" ]]; then
-  git config --global user.name "$MERADEB_USER_NAME"
-fi
-
-if [[ -n "${MERADEB_USER_EMAIL//[[:space:]]/}" ]]; then
-  git config --global user.email "$MERADEB_USER_EMAIL"
-fi
