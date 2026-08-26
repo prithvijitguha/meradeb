7a094896adec8ca1d35d33661a1215c3fadf0082
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-08-26 22:57:19 +0530

    Removed uninstall, debloat

--- bin/meradeb-sub/uninstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-UNINSTALLER=$(gum file $MERADEB_PATH/uninstall --height 26)
-[ -n "$UNINSTALLER" ] && gum confirm "Run uninstaller?" && source $UNINSTALLER && gum spin --spinner globe --title "Uninstall completed!" -- sleep 3
-clear
-source $MERADEB_PATH/bin/meradeb
