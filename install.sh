b3b775379305d9704bf6a109837b548c5662063e
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-08-26 21:16:35 +0530

    Removed first run choices, identification and update

--- install.sh
+++ install.sh
@@ -12,8 +12,6 @@ source ~/.local/share/meradeb/install/check-version.sh
 # Ask for app choices
 echo "Get ready to make a few choices..."
 source ~/.local/share/meradeb/install/terminal/required/app-gum.sh >/dev/null
-source ~/.local/share/meradeb/install/first-run-choices.sh
-source ~/.local/share/meradeb/install/identification.sh
 
 # Desktop software and tweaks will only be installed if we're running Gnome
 if [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]]; then
