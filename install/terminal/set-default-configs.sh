b39be244490a0bee99d8767d9d294d55453c6b85
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-08-26 21:42:54 +0530

    source zshrc file

--- install/terminal/set-default-configs.sh
+++ install/terminal/set-default-configs.sh
@@ -10,5 +10,3 @@ cp ~/.local/share/meradeb/configs/.tmux.conf ~/.tmux.conf
 mkdir -p ~/.config/btop/themes
 cp ~/.local/share/meradeb/configs/btop.conf ~/.config/btop/btop.conf
 cp ~/.local/share/meradeb/themes/tokyo-night/btop.theme ~/.config/btop/themes/tokyo-night.theme
-
-source ~/.zshrc
