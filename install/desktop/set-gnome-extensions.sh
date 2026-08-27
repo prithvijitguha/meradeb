19a764ab99aa1127b04e07ae1d7a544f89f4721c
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-08-27 19:00:16 +0530

    Moved settings to gnome extension settings

--- install/desktop/set-gnome-extensions.sh
+++ install/desktop/set-gnome-extensions.sh
@@ -68,3 +68,18 @@ gsettings set org.gnome.shell.extensions.space-bar.shortcuts open-menu "@as []"
 
 # Enable the system-monitor
 gnome-extensions enable system-monitor@gnome-shell-extensions.gcampax.github.com
+
+# Center new windows in the middle of the screen
+gsettings set org.gnome.mutter center-new-windows true
+
+# Set Cascadia Mono as the default monospace font
+gsettings set org.gnome.desktop.interface monospace-font-name 'CaskaydiaMono Nerd Font 10'
+
+# Turn off ambient sensors for setting screen brightness (they rarely work well!)
+gsettings set org.gnome.settings-daemon.plugins.power ambient-enabled false
+
+# System monitor
+gsettings set org.gnome.shell.extensions.system-monitor show-cpu true
+gsettings set org.gnome.shell.extensions.system-monitor show-download true
+gsettings set org.gnome.shell.extensions.system-monitor show-upload true
+gsettings set org.gnome.shell.extensions.system-monitor show-memory true
