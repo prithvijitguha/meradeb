431428f96fb00a7d9ce74febb78afb90bc0be6f6
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-08-28 19:23:38 +0530

    Added missed flatpak packages

--- install/terminal/install-all-terminal-packages.sh
+++ install/terminal/install-all-terminal-packages.sh
@@ -8,3 +8,4 @@ source ~/.local/share/meradeb/install/terminal/app-cli-tools.sh
 source ~/.local/share/meradeb/install/terminal/app-fastfetch.sh
 source ~/.local/share/meradeb/install/terminal/app-omz.sh
 source ~/.local/share/meradeb/install/terminal/set-default-configs.sh
+source ~/.local/share/meradeb/install/terminal/install-flatpak-packages.sh
