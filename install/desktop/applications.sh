8cc1b03b591f7d4c1f79be0238e5de07d0150038
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-08-28 19:24:47 +0530

    Updated applications echo and removed applications

--- install/desktop/applications.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Installing all applications"
-for script in ~/.local/share/meradeb/applications/*.sh; do source $script; done
--- install/install-applications.sh
+++ install/install-applications.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+echo "Installing all applications"
 for script in ~/.local/share/meradeb/applications/*.sh; do source $script; done
