3e4378e55e04e4ede6912b4afaa5fce52adba556
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-08-29 23:39:29 +0530

    Change to everforest colorscheme

--- themes/everforest/gnome.sh
+++ themes/everforest/gnome.sh
@@ -3,3 +3,5 @@
 MERADEB_THEME_COLOR="bark"
 MERADEB_THEME_BACKGROUND="everforest.jpg"
 source $MERADEB_PATH/themes/set-gnome-theme.sh
+# Set the gnome shell setting color
+gsettings set org.gnome.shell.extensions.tilingshell window-border-color "#78ab50"
