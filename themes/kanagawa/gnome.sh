05df9a8b05ecdd1bcb5f673cda9c3d9281885bb6
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-08-30 07:46:42 +0530

    Updated themes for gnome border

--- themes/kanagawa/gnome.sh
+++ themes/kanagawa/gnome.sh
@@ -3,3 +3,4 @@
 MERADEB_THEME_COLOR="purple"
 MERADEB_THEME_BACKGROUND="kanagawa.jpg"
 source $MERADEB_PATH/themes/set-gnome-theme.sh
+gsettings set org.gnome.shell.extensions.tilingshell window-border-color "#924d8b"
--- themes/matte-black/gnome.sh
+++ themes/matte-black/gnome.sh
@@ -3,3 +3,4 @@
 MERADEB_THEME_COLOR="orange"
 MERADEB_THEME_BACKGROUND="matte-black.jpg"
 source $MERADEB_PATH/themes/set-gnome-theme.sh
+gsettings set org.gnome.shell.extensions.tilingshell window-border-color "#FFFFFF"
