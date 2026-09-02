69f864b693138818f0229b64adea73a370c28029
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-09-02 21:28:40 +0530

    Replaced echos with gum log

--- install/install-base-packages.sh
+++ install/install-base-packages.sh
@@ -3,6 +3,6 @@ sudo apt update -y
 sudo apt upgrade -y
 
 # Then install all other files
-echo "Installing following base packages"
-echo "$(cat ~/.local/share/meradeb/install/meradeb-base-packages.txt)"
+gum log --structured --level info "Installing following base packages"
+gum log --structured --level info "$(cat ~/.local/share/meradeb/install/meradeb-base-packages.txt)"
 sudo apt install -y $(cat ~/.local/share/meradeb/install/meradeb-base-packages.txt)
