886ac4b620571bf908f7ad855c7bcf1075ca2727
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-09-02 18:36:32 +0530

    Removed yes, had issues

--- install/desktop/set-gnome-extensions.sh
+++ install/desktop/set-gnome-extensions.sh
@@ -3,13 +3,13 @@
 pipx install gnome-extensions-cli --system-site-packages
 
 # Install new extensions
-yes | gext install tilingshell@ferrarodomenico.com
-yes | gext install tactile@lundal.io
-yes | gext install just-perfection-desktop@just-perfection
-yes | gext install blur-my-shell@aunetx
-yes | gext install space-bar@luchrioh
-yes | gext install tophat@fflewddur.github.io
-yes | gext install auto-move-windows@gnome-shell-extensions.gcampax.github.com
+gext install tilingshell@ferrarodomenico.com
+gext install tactile@lundal.io
+gext install just-perfection-desktop@just-perfection
+gext install blur-my-shell@aunetx
+gext install space-bar@luchrioh
+gext install tophat@fflewddur.github.io
+gext install auto-move-windows@gnome-shell-extensions.gcampax.github.com
 
 # Compile gsettings schemas
 sudo cp ~/.local/share/gnome-shell/extensions/tilingshell@ferrarodomenico.com/schemas/org.gnome.shell.extensions.tilingshell.gschema.xml \
