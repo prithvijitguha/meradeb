35b12b8f9dfd978624f8f6eaaec239945ec93b4c
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-09-02 18:47:32 +0530

    Updated build docs and manual reference

--- bin/meradeb-sub/manual.sh
+++ bin/meradeb-sub/manual.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-chromium --app="file:///$MERADEB_PATH/docs/build/html/manual.html" --name=Manual --class=Manual &>/dev/null
+chromium --app="file:///$MERADEB_PATH/docs/build/html/index.html" --name=Manual --class=Manual &>/dev/null
 source $MERADEB_PATH/bin/meradeb-sub/menu.sh
