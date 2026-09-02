c262fe4a17ff79cc1e67eca6296a07adf52e4bb5
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-09-02 19:26:12 +0530

    Updated docs

--- bin/meradeb-sub/manual.sh
+++ bin/meradeb-sub/manual.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-chromium --app="file:///$MERADEB_PATH/docs/build/index.html" --name=Manual --class=Manual &>/dev/null
-source $MERADEB_PATH/bin/meradeb-sub/menu.sh
+cd "$MERADEB_PATH" || exit 1
+
+mkdocs serve \
+  --dev-addr 127.0.0.1:8000 \
+  >/dev/null 2>&1 &
+
+chromium \
+  --app="http://127.0.0.1:8000/" \
+  --name=Manual \
+  --class=Manual \
+  &>/dev/null &
+
+source "$MERADEB_PATH/bin/meradeb-sub/menu.sh"
