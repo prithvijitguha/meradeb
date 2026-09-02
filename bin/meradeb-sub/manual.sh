06699ec0385d05967fe2038189f421c47adbfffb
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-09-02 19:49:23 +0530

    Updated port

--- bin/meradeb-sub/manual.sh
+++ bin/meradeb-sub/manual.sh
@@ -3,11 +3,11 @@
 cd "$MERADEB_PATH" || exit 1
 
 mkdocs serve \
-  --dev-addr 127.0.0.1:8000 \
+  --dev-addr 127.0.0.1:8080 \
   >/dev/null 2>&1 &
 
 chromium \
-  --app="http://127.0.0.1:8000/" \
+  --app="http://127.0.0.1:8080/" \
   --name=Manual \
   --class=Manual \
   &>/dev/null &
