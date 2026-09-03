d75a7650b1428b4bfc9d7902d40d83c7f15f8bab
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-09-03 10:19:31 +0530

    Removed hotkey for shutdown

--- install/desktop/set-gnome-hotkeys.sh
+++ install/desktop/set-gnome-hotkeys.sh
@@ -10,11 +10,6 @@ gsettings set org.gnome.desktop.wm.keybindings minimize "['<Super>h']"
 # Full-screen with title/navigation bar
 gsettings set org.gnome.desktop.wm.keybindings toggle-fullscreen "['<Shift>F11']"
 
-# Shutdown
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/shutdown/ name 'Shutdown'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/shutdown/ command 'systemctl poweroff'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/shutdown/ binding '<Super><Shift>s'
-
 # Use 6 fixed workspaces instead of dynamic mode
 gsettings set org.gnome.mutter dynamic-workspaces false
 gsettings set org.gnome.desktop.wm.preferences num-workspaces 6
