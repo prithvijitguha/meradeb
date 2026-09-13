72fb661fdbae5a3430d1603b706dd0a147cdb214
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-09-13 15:30:01 +0530

    Updated fastfetch

--- install/terminal/set-default-configs.sh
+++ install/terminal/set-default-configs.sh
@@ -5,6 +5,7 @@ cp ~/.local/share/meradeb/configs/.p10.zsh ~/.p10.zsh
 cp ~/.local/share/meradeb/configs/.profile ~/.profile
 cp ~/.local/share/meradeb/configs/.zshrc ~/.zshrc
 cp ~/.local/share/meradeb/configs/.tmux.conf ~/.tmux.conf
+cp -R ~/.local/share/meradeb/configs/* ~/.config/*/
 
 # Use meradeb btop config
 mkdir -p ~/.config/btop/themes
