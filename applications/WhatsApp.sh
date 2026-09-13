3d4fe3d2d8c686c867fac51d0ff435c22f46496d
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-09-13 16:53:29 +0530

    updated installation of applications

--- applications/WhatsApp.sh
+++ applications/WhatsApp.sh
@@ -11,5 +11,6 @@ Type=Application
 Icon=/home/$USER/.local/share/meradeb/applications/icons/WhatsApp.png
 Categories=GTK;
 MimeType=text/html;text/xml;application/xhtml_xml;
-StartupNotify=true;
+StartupNotify=true
+StartupWMClass=Whatsapp
 EOF
