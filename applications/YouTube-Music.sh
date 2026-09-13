fd29a0a65630efb6d1443cfb18633ef37f47ee16
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-09-13 17:20:07 +0530

    Fixed whatsapp and youtube music

--- applications/YouTube-Music.sh
+++ applications/YouTube-Music.sh
@@ -11,5 +11,5 @@ Type=Application
 Icon=$HOME/.local/share/meradeb/applications/icons/Music.png
 Categories=GTK;
 StartupNotify=true
-StartupWMClass=YouTubeMusic
+StartupWMClass=chrome-music.youtube.com__-Default
 EOF
