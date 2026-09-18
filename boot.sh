a5b4b80c9461970b016f584f17ff78d782b771b0
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-09-18 22:00:47 +0530

    Updated book

--- boot.sh
+++ boot.sh
@@ -35,11 +35,17 @@ echo "=> Warning: Meradeb is recommeded for Debian 13 Trixie setups"
 echo -e "\nBegin installation (or abort with ctrl+c)..."
 
 echo "Cloning Meradeb..."
-rm -rf ~/.local/share/meradeb
-git clone git@github.com:prithvijitguha/meradeb.git ~/.local/share/meradeb
+if [[ -d ~/.local/share/meradeb/.git ]]; then
+  cd ~/.local/share/meradeb
+  git fetch origin
+  git pull --ff-only
+else
+  git clone git@github.com:prithvijitguha/meradeb.git ~/.local/share/meradeb
+fi
+
 if [[ $MERADEB_REF != "master" ]]; then
   cd ~/.local/share/meradeb
-  git fetch origin "${MERADEB_REF:-stable}" && git checkout "${MERADEB_REF:-stable}"
+  git fetch origin "${MERADEB_REF:-main}" && git checkout "${MERADEB_REF:-main}"
   cd -
 fi
 
