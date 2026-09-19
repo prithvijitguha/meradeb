d694dc0ae622f8d15cc5bd959703412a899d8994
Prithvijit <71138854+prithvijitguha@users.noreply.github.com>
2026-09-19 19:32:16 +0530

    Install curl before setting up Gum repository
    Added installation of curl before adding the Charm repository.

--- install/terminal/required/app-gum.sh
+++ install/terminal/required/app-gum.sh
@@ -3,6 +3,7 @@
 # Gum is used for the meradeb commands for tailoring meradeb after the initial install
 if ! command -v gum >/dev/null 2>&1; then
   sudo mkdir -p /etc/apt/keyrings
+  sudo apt update && sudo apt install curl
   curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
   echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
   sudo apt update && sudo apt install gum
