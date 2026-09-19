22580320567a78d01e5ae313c1fe1796441de538
Prithvijit Guha <71138854+prithvijitguha@users.noreply.github.com>
2026-09-19 22:59:53 +0530

    Fixed default config install

--- install/terminal/set-default-configs.sh
+++ install/terminal/set-default-configs.sh
@@ -2,7 +2,8 @@
 
 # Setup default configs
 cp -a ~/.local/share/meradeb/configs/. ~/.config/
-
+# TODO:: Change before meradeb merge
+chezmoi init --branch feature-only-configs https://github.com/prithvijitguha/dotfiles.git
 # Use meradeb btop config
 mkdir -p ~/.config/btop/themes
 cp ~/.local/share/meradeb/configs/btop.conf ~/.config/btop/btop.conf
