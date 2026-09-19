b7b097acc9132f1b059254fab999daa6b67a1e80
Prithvijit Guha <71138854+prithvijitguha@users.noreply.github.com>
2026-09-20 01:55:44 +0530

    Updated default commands

--- install/terminal/set-default-configs.sh
+++ install/terminal/set-default-configs.sh
@@ -4,13 +4,14 @@ mkdir -p ~/.config
 mkdir -p ~/.config/btop
 mkdir -p ~/.config/btop/themes
 
-cp ~/.local/share/meradeb/configs/.bashrc ~/.config/.bashrc
-cp ~/.local/share/meradeb/configs/.zshrc ~/.config/.zshrc
-cp ~/.local/share/meradeb/configs/.p10.zsh ~/.config/.p10.zsh
-cp ~/.local/share/meradeb/configs/.profile ~/.config/.profile
-cp ~/.local/share/meradeb/configs/.tmux.conf ~/.config/.tmux.conf
-cp ~/.local/share/meradeb/configs/.vimrc ~/.config/.vimrc
+cp ~/.local/share/meradeb/configs/.bashrc ~/.bashrc
+cp ~/.local/share/meradeb/configs/.zshrc ~/.zshrc
+cp ~/.local/share/meradeb/configs/.p10.zsh ~/.p10.zsh
+cp ~/.local/share/meradeb/configs/.profile ~/.profile
+cp ~/.local/share/meradeb/configs/.tmux.conf ~/.tmux.conf
+cp ~/.local/share/meradeb/configs/.vimrc ~/.vimrc
 cp ~/.local/share/meradeb/configs/btop.conf ~/.config/btop/btop.conf
+
 cp -r ~/.local/share/meradeb/configs/.vim ~/.config/.vim
 cp -r ~/.local/share/meradeb/configs/fastfetch ~/.config/fastfetch
 cp -r ~/.local/share/meradeb/configs/ghostty ~/.config/ghostty
