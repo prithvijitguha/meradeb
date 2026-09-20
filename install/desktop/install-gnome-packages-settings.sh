2100b657391cfaa68442df058cba03a2568c61d9
Prithvijit Guha <71138854+prithvijitguha@users.noreply.github.com>
2026-09-20 13:28:33 +0530

    Minor for theme directory

--- install/desktop/install-gnome-packages-settings.sh
+++ install/desktop/install-gnome-packages-settings.sh
@@ -3,8 +3,8 @@
 source ~/.local/share/meradeb/install/desktop/set-gnome-extensions.sh
 source ~/.local/share/meradeb/install/desktop/fonts.sh
 # set the everforest theme to start
-WALLPAPER_PATH="$HOME/.local/share/meradeb/install/themes/everforest/backgrounds/1-tree-tops.webp"
-WALLPAPER_DIR="$HOME/.local/share/meradeb/install/themes/everforest"
+WALLPAPER_PATH="$HOME/.local/share/meradeb/themes/everforest/backgrounds/1-tree-tops.webp"
+WALLPAPER_DIR="$HOME/.local/share/meradeb/themes/everforest"
 THEME="everforest"
 source ~/.local/share/meradeb/themes/set-gnome-theme.sh
 
