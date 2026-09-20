6c014178ca9236f02f5a6cd102f4e264d2af6e39
Prithvijit Guha <71138854+prithvijitguha@users.noreply.github.com>
2026-09-20 13:57:06 +0530

    test for chromaleon, removed commented out code

--- themes/set-gnome-theme.sh
+++ themes/set-gnome-theme.sh
@@ -2,11 +2,6 @@
 
 gum log --structured --level info "Installing Gnome theme $THEME"
 
-# COLORS="$WALLPAPER_DIR/colors.toml"
-
-# get_color() {
-#   sed -n "s/^$1 = \"\\(#[0-9a-fA-F]*\\)\"$/\1/p" "$COLORS"
-# }
 BACKGROUND_DEST_DIR="$HOME/.local/share/backgrounds"
 
 mkdir -p $BACKGROUND_DEST_DIR
@@ -20,6 +15,9 @@ set_background() {
   dest="$BACKGROUND_DEST_DIR/$name"
 
   [ -f "$dest" ] || cp "$background" "$dest"
+  # Clear first so a "changed" signal always fires, even for the same wallpaper
+  gsettings reset org.gnome.desktop.background picture-uri
+  gsettings reset org.gnome.desktop.background picture-uri-dark
 
   gsettings set org.gnome.desktop.background picture-uri "$dest"
   gsettings set org.gnome.desktop.background picture-uri-dark "$dest"
@@ -33,12 +31,3 @@ set_background "$WALLPAPER_PATH"
 if [ -f "$WALLPAPER_DIR/gnome.sh" ]; then
   source "$WALLPAPER_DIR/gnome.sh"
 fi
-
-# gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-# gsettings set org.gnome.desktop.interface cursor-theme 'Yaru'
-# gsettings set org.gnome.desktop.interface gtk-theme "Yaru-$MERADEB_THEME_COLOR-dark"
-# gsettings set org.gnome.desktop.interface icon-theme "Yaru-$MERADEB_THEME_COLOR"
-# gsettings set org.gnome.desktop.interface accent-color "$MERADEB_THEME_COLOR"
-
-# Tophat
-# gsettings set org.gnome.shell.extensions.tophat meter-fg-color "$(get_color accent)"
